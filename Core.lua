eae119206e6342c32875bc1b490519846e446806
obscurelyme <nico.frozencoffee@gmail.com>
2022-07-22 15:12:13 -0700

    update version and fixes to args

--- Core.lua
+++ Core.lua
@@ -1,4 +1,5 @@
-Obscurity = LibStub("AceAddon-3.0"):NewAddon("Obscurity", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0")
+Obscurity = LibStub("AceAddon-3.0"):NewAddon("Obscurity", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0")
+local COMM_PREFIX = "OBS_COMM"
 
 function Obscurity:OnInitialize()
   -- Called when the addon is loaded
@@ -10,7 +11,7 @@ function Obscurity:OnEnable()
   -- Called when the addon is enabled
   self:RegisterEvent("ENCOUNTER_START", "OnEncounterStart")
   self:RegisterEvent("ENCOUNTER_END", "OnEncounterEnd")
-  self:RegisterComm("ObscurityPingTest", "OnCommReceived")
+  self:RegisterComm(COMM_PREFIX, "OnCommReceived")
 end
 
 function Obscurity:OnDisable()
@@ -22,30 +23,19 @@ function Obscurity:SlashCommandProc(msg)
     self:Disable()
   elseif msg == "enable" then
     self:Enable()
-  elseif msg == "ex" then
-    self:Extra()
-  elseif msg == "time" then
-    self:Print(self:GetCurrentEncounterTime())
-  elseif msg == "start" then
-    self:OnEncounterStart(nil, "123", "Test Encounter")
-    WeakAuras.ScanEvents("OBS_ENCOUNTER_START")
   elseif msg == "end" then
     self:OnEncounterEnd(nil, "123", "Test Encounter")
     WeakAuras.ScanEvents("OBS_ENCOUNTER_END")
   elseif msg == "wipe" then
-    WeakAuras.ScanEvents("OBSCURITY_WIPE")
+    self:SendComm("OBSCURITY_WIPE")
   elseif msg == "dh" then
-    WeakAuras.ScanEvents("OBSCURITY_DIVINE_HYMN")
+    self:SendComm("OBSCURITY_COOLDOWN", "OBSCURITY_DIVINE_HYMN")
   elseif msg == "bait" then
-    WeakAuras.ScanEvents("OBSCURITY_BAIT")
+    self:SendComm("OBSCURITY_BAIT")
   elseif msg == "stack" then
-    WeakAuras.ScanEvents("OBSCURITY_STACK")
+    self:SendComm("OBSCURITY_STACK")
   elseif msg == "soak" then
-    WeakAuras.ScanEvents("OBSCURITY_SOAK")
-  elseif msg == "gs" then
-    WeakAuras.ScanEvents("OBSCURITY_GUARDIAN_SPIRIT", target)
-  elseif msg == "ping" then
-    self:SendCommMessage("ObscurityPingTest", "Hello, World!", )
+    self:SendComm("OBSCURITY_SOAK")
 	else
     if (self:IsEnabled()) then
       self:Print("Obscurity v0.0.1 is currently: ENABLED")
@@ -53,6 +43,27 @@ function Obscurity:SlashCommandProc(msg)
       self:Print("Obscurity v0.0.1 is currently: DISABLED")
     end
 	end
+end
+
+function Obscurity:SendComm(command, arg1, arg2, arg3)
+  print("SendComm Params", command, arg1, arg2, arg3)
+  local serializedPayload = self:SerializeCommand(command, arg1, arg2, arg3)
+  if IsInRaid() == true then
+    self:SendCommMessage(COMM_PREFIX, serializedPayload, "RAID", nil, "NORMAL")
+  end
+
+  if UnitInParty("player") then
+    self:SendCommMessage(COMM_PREFIX, serializedPayload, "PARTY", nil, "NORMAL")
+  end
+end
+
+function Obscurity:SerializeCommand(command, arg1, arg2, arg3)
+  return self:Serialize({
+    command = command,
+    arg1 = arg1,
+    arg2 = arg2,
+    arg3 = arg3
+  })
 end
 
 function Obscurity:OnEncounterStart(eventName, encounterID, encounterName, difficultyID, groupSize, success)
@@ -63,6 +74,47 @@ function Obscurity:OnEncounterEnd(eventName, encounterID, encounterName, difficu
   self:Print("Ended encounter " .. encounterName)
 end
 
-function Obscurity:OnCommReceived(params, ...)
-  self:Print(params)
+function Obscurity:OnCommReceived(prefix, payload, distro, sender)
+  local success, deserialized = self:Deserialize(payload)
+  if success == true then
+    -- We have information about the command to run, we need to parse it and then tell WeakAuras about it.
+    self:ParseCommand(deserialized.command, deserialized.arg1, deserialized.arg2, deserialized.arg3, sender)
+  else
+    self:Print("Could not deserialize payload from recent comm")
+  end
+end
+
+function Obscurity:ParseCommand(command, arg1, arg2, arg3, sender)
+  -- TODO: check if the sender has authority to give commands (IE: Raid lead or assist)
+  -- UnitIsRaidOfficer
+  -- GetRaidRosterInfo and then use that to UnitGUID("unit")
+  if Obscurity[command] ~= nil then
+    self[command](command, arg1, arg2, arg3)
+  end
+end
+
+function Obscurity:OBSCURITY_WIPE()
+  WeakAuras.ScanEvents("OBSCURITY_WIPE")
+end
+
+function Obscurity:OBSCURITY_BAIT()
+  WeakAuras.ScanEvents("OBSCURITY_BAIT")
+end
+
+function Obscurity:OBSCURITY_STACK()
+  WeakAuras.ScanEvents("OBSCURITY_STACK")
+end
+
+function Obscurity:OBSCURITY_SOAK()
+  WeakAuras.ScanEvents("OBSCURITY_SOAK")
+end
+
+function Obscurity:OBSCURITY_SPREAD()
+  WeakAuras.ScanEvents("OBSCURITY_SPREAD")
+end
+
+function Obscurity:OBSCURITY_COOLDOWN(arg1, arg2, arg3)
+  if arg1 ~= nil then
+    WeakAuras.ScanEvents(arg1, arg2, arg3)
+  end
 end
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Obscurity Raid Callouts
 
 This is a simple backend addon for raid leads and raid assitants to perform real time speech to text/visual cues during raid and dungeon boss encounters.
+
+This addon relies on various macros to send comms over the wire to listeners in order to send/receive instructions about what to do during the raid.
+
+Standard format for `SendComm` looks like this...
+
+```
+Obscurity:SendComm([COMMAND], [COOLDOWN?], [TARGET?], [ARG2?], [ARG3?])
+```
+
+Where Cooldown, target, args2 and 3 are totally optional
+
+### Macro Examples
+
+The following macro is an example on how to call for Guardian Spirit CD on your mouseover target.
+
+```
+/run Obscurity:SendComm("OBSCURITY_COOLDOWN", "OBSCURITY_GUARDIAN_SPIRIT", UnitName("mouseover"))
+```
+
+Here is another way to call for a cooldown without asking for a particular target in mind
+
+```
+/run Obscurity:SendComm("OBSCURITY_COOLDOWN", "OBSCURITY_TRANQUILITY")
+```
+
+Other helpful raid callouts include...
+- **Bait**: OBSCURITY_BAIT - Calls to bait, raid/party member should be aware of what "bait" means in the context of the encounter
+- **Soak**: OBSCURITY_SOAK - Calls to soak, raid/party member should be aware of what "soak" means in the context of the encounter
+- **Stack**: OBSCURITY_STACK - Calls to stack, raid/party member should be aware of what "stack" means in the context of the encounter
+- **Wipe**: OBSCURITY_WIPE - Calls for a raid wipe
+- **Spread**: OBSCURITY_SPREAD - Calls to spread out
+- **Cooldown**: OBSCURITY_COOLDOWN - Calls for a Specific Cooldown, optionally can call for a CD to be used on a specific raid/party member
